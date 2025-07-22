e3a27624a21ee75118efe482efa1991e83169f41
EZacS <43499834+EZacS@users.noreply.github.com>
2025-07-22 11:43:36 +0200

    add mouse wiggler template

---
+++ mouse-wiggler-1.21.8/src/client/java/dev/iasc/mousewiggler/MouseWigglerClient.java
@@ -0,0 +1,10 @@
+package dev.iasc.mousewiggler;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MouseWigglerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
