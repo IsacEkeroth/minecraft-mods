c3360e9039a860234337f463d514e0368471d41c
EZacS <43499834+EZacS@users.noreply.github.com>
2025-07-22 11:43:13 +0200

    add autotyper wip

---
+++ autotyper-1.21.8/src/client/java/dev/iasc/autotyper/AutoTyperClient.java
@@ -0,0 +1,11 @@
+package dev.iasc.autotyper;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.network.ClientPlayerEntity;
+
+public class AutoTyperClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
